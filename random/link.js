3977e27a5ab5b6d9b37cfc28124e0b402722dc90
Twttsunami <53795032+Twttsunami@users.noreply.github.com>
2019-08-06 15:54:30 -0500

    link.js
    Corrected :)

--- random/link.js
+++ random/link.js
@@ -128,7 +128,8 @@ const goals = ['slaying', 'wisdom', 'destruction', 'death', 'healing', 'luck', '
   'leading', 'ruling', 'command', 'integrity', 'humility', 'honour', 'virtue', 'completeness', 'unity', 'focus', 'training',
   'knowledge', 'restoration', 'refurbishment', 'disclosure', 'energy', 'violation', 'stringency', 'loath', 'fuss', 'megalomania',
   'danger', 'gallivanting', 'hibernation', 'horror', 'terror', 'eminence', 'shame', 'scorn', 'ingenuinity', 'paroxysm', 'satiety',
-  'placidity', 'phantasmagoria', 'euphoria', 'bad']
+  'placidity', 'phantasmagoria', 'euphoria', 'bad', 'sympathetic', 'slow', 'rigid', 'kind', 'intrepid', 'impulsive', 'anxious', 'modern',
+  'think', 'noisy', 'small', 'thing', 'happy', 'huge', 'mature']
 
 export const generate = ()=>{
   // generates a random party invite link name
